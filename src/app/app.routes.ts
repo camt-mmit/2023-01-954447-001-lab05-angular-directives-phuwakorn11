a1d63a2e9e0589789acd4413fce1bff1ebfba72d
Kritsada Intaprom <kritsada.itp@gmail.com>
2023-07-24 17:02:34 +0700

    week05

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { TryOneComponent } from './try-one/try-one.component';
+import { TryTwoComponent } from './try-two/try-two.component';
+import { TryThreeComponent } from './try-three/try-three.component';
+export const routes: Routes = [
+  {path: 'try-one',component: TryOneComponent},
+  {path: 'try-two',component: TryTwoComponent},
+  {path: 'try-three',component: TryThreeComponent}
+];
